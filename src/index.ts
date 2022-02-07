284ffddcf4f841b576739c79b4f4a2eca65ab840
Simon Skinner <vultuk28@gmail.com>
2022-02-07 20:41:45 +0000

    Work

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import {
   Microservice as HttpMicroservice,
   Route,
-} from "@vultuk/microservice-http";
-import { ApolloServer } from "apollo-server-express";
-import { Settings } from "./types/graphQLSettings";
-import { SchemaResolvers } from "./types/schemaResolver";
+} from '@vultuk/microservice-http';
+import { ApolloServer } from 'apollo-server-express';
+import { Settings } from './types/graphQLSettings';
+import { SchemaResolvers } from './types/schemaResolver';
 
-export * from "./types/graphQLSettings";
+export * from './types/graphQLSettings';
 
 export const Microservice =
   (settings?: Settings) =>
@@ -29,15 +29,15 @@ export const Microservice =
       },
     });
 
-    server.start();
-
-    return HttpMicroservice(settings)(middleware, [
-      ...(appOnlyMiddleware || []),
-      (app: any) => {
-        server.applyMiddleware({
-          app,
-          path: settings?.path || "/",
-        });
-      },
-    ])(routes);
+    return server.start().then(() => {
+      return HttpMicroservice(settings)(middleware, [
+        ...(appOnlyMiddleware || []),
+        (app: any) => {
+          server.applyMiddleware({
+            app,
+            path: settings?.path || '/',
+          });
+        },
+      ])(routes);
+    });
   };
