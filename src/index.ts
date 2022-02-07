bc01e808361c518ef284f66396bc907b23093101
Simon Skinner <vultuk28@gmail.com>
2022-02-07 17:44:36 +0000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import {
   Microservice as HttpMicroservice,
   Route,
-} from "@vultuk/microservice-http";
-import { Settings } from "./types/graphQLSettings";
-import { SchemaResolvers } from "./types/schemaResolver";
+} from '@vultuk/microservice-http';
+import { Settings } from './types/graphQLSettings';
+import { SchemaResolvers } from './types/schemaResolver';
 
-export * from "./types/graphQLSettings";
+export * from './types/graphQLSettings';
 
 export const Microservice =
   (settings?: Settings) =>
   (middleware?: any[], appOnlyMiddleware?: any[]) =>
   (routes: Route[], schemaResolvers?: SchemaResolvers) => {
+    routes.forEach((route) => {
+      // Check we aren't trying to use GraphQL at the same time as another Route
+      if (route.path === settings.path) {
+        throw new Error(
+          `Can't set up GraphQL on the same route as a ${route.method} route`
+        );
+      }
+    });
+
     return HttpMicroservice(settings)(middleware, appOnlyMiddleware)(routes);
   };
