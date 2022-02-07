5d5eb3411e570584b3e87c3f55a3b008ec548858
Simon Skinner <vultuk28@gmail.com>
2022-02-07 17:41:31 +0000

    1.2.1

--- src/types/schemaResolver.ts
+++ src/types/schemaResolver.ts
@@ -1,8 +1,8 @@
-import type { IExecutableSchemaDefinition } from '@graphql-tools/schema';
+import type { IExecutableSchemaDefinition } from "@graphql-tools/schema";
 
 export type SchemaResolver = {
-  schema: IExecutableSchemaDefinition['typeDefs'];
-  resolver: IExecutableSchemaDefinition['resolvers'];
+  schema: IExecutableSchemaDefinition["typeDefs"];
+  resolver: IExecutableSchemaDefinition["resolvers"];
 };
 
 export type SchemaResolvers = SchemaResolver[];
